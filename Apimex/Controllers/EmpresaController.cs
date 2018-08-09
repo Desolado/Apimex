146c181f421d6eacef65a78f053d53a809ae7f9e
Eduardo Perez Camacho <kamacho_lalo@hotmail.com>
2018-08-09 00:03:14 -0500

    efgs

---
+++ Apimex/Controllers/EmpresaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Apimex.Models;
+
+namespace Apimex.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: Empresa
+        
+
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Empresas E)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new ApimexEntities())
+                {
+                    E.FechaRegistro = DateTime.Now;
+                    E.Folio = "FE-"+E.id_Empresa;
+                    db.Empresas.Add(E);
+                    db.SaveChanges();
+                    return RedirectToAction("Create");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("","Error al registrarse.");
+                return View();
+            }
+        }
+    }
+}
---
+++ Apimex/Models/EmpresasCE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Apimex.Models
+{
+    public class EmpresasCE
+    {
+        public long id_Empresa { get; set; }
+        [Required]
+        [Display(Name = "Ingrese el nombre de la empresa.")]
+        public string Nombre { get; set; }
+        public string Razon { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Folio { get; set; }
+        public Nullable<System.DateTime> FechaRegistro { get; set; }
+    }
+}
